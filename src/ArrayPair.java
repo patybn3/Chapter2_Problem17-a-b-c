34d2f90693c0a1ef7e3c1cfb24c4caeac51449d7
patybn3 <paty.nigro@gmail.com>
2022-09-29 16:54:47 -0400

    File ArrayPair is supposed to take an array of size 2 for both objects passed. Test not ran yet.

--- src/ArrayPair.java
+++ src/ArrayPair.java
@@ -1,25 +1,31 @@
 public class ArrayPair<T> implements PairInterface<T>
 {
-    protected T[] firstObj;
-    protected T[] secondObj;
+    protected T arrayOfObj[];
+
+    public ArrayPair(T setFirstObj, T setSecObj)
+    {
+        arrayOfObj = (T[]) new Object[2];
+        arrayOfObj[0] = setFirstObj;
+        arrayOfObj[1] = setFirstObj;
+    }
 
     @Override
     public void setFirst(T first) {
-
+        arrayOfObj[0] = first;
     }
 
     @Override
     public void setSecond(T second) {
-
+        arrayOfObj[1] = second;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        return arrayOfObj[0];
     }
 
     @Override
     public T getSecond() {
-        return null;
+        return arrayOfObj[1];
     }
 }
